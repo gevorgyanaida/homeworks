4401673cd551fdb4fa56d8376d4326161dc2d3fc
gevorgyanaida <99826164+gevorgyanaida@users.noreply.github.com>
2022-04-24 15:17:26 +0400

    Add files via upload

--- Homework2.js
+++ Homework2.js
@@ -1,57 +1,79 @@
-// 1. For a given number calculate the sum of its digits.
+//1. Given an array of numbers which is almost sorted in ascending order. Find the index where sorting order is violated.
 
-let value = +prompt('Tiv')
-let sum = 0
-while(value){
-    sum += value%10
-    value = Math.floor(value/10)
-}
-console.log(sum)
-
-// 2. Given three sides of a triangle. Check whether the triangle is valid or not. Print “yes” if it is valid and “no”; otherwise. (Triangle is valid if the sum of its two sides are greater than the third side).
-
-let side1 = +prompt('Erkarutyun')
-let side2 = +prompt('Erkarutyun')
-let side3 = +prompt('Erkarutyun')
-if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1){
-    console.log('Yes')
-} else {
-    console.log('No')
-}
-
-// 3. Given a number print its digits count.
-
-let number = +prompt('Tiv')
-let numOfDigits = 0
-if (number===0) {
-    console.log(1)
-} else {
-    while (number) {
-        numOfDigits++
-        number = (number-number%10)/10
+function orderViol(arr){
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] >= arr[i + 1]){
+            return i + 1
+        }     
     }
-    console.log(numOfDigits)
+    return -1
 }
 
-// 4. Count numbers of digit 9 in a number.
+//2. Given an array consisting from the arrays of numbers (like a two-dimensional array). Find sum of each row and print them as an array
 
-let number = +prompt('Tiv')
-let numberOfDigit9 = 0
-for (;number!==0;number=(number-number%10)/10) {
-    if (number%10===9) {
-        numberOfDigit9++
+function sum (arr){
+    let sum = 0
+    for(let i = 0; i < arr.length; i++){
+        sum += arr[i]
     }
+    return sum
 }
-console.log(numberOfDigit9)
 
-// 5. Given a positive number. Print it in reverse order.
-
-let num = +prompt('Tiv')
-let rev = 0
-let lastDigit 
-while (num!==0) {
-    lastDigit = num % 10
-    rev = rev*10 + lastDigit
-    num = Math.floor(num/10)
+function sumOfArr(arr){
+    let sumArr = []
+    for(let i = 0; i < arr.length; i++){
+       sumArr.push(sum(arr[i]))
+    }
+    return sumArr
 }
-console.log(rev)
+
+/*3. Given an array of integers. Write a function that return new array from first array,
+where removed even numbers, and odd numbers was multiplied with new array length */
+
+function newArr(arr){
+    function odd(el){
+       if (el % 2){
+           return true
+       }
+    }
+       let res = arr.filter(odd).map(function(el){
+       return el * arr.filter(odd).length
+   })
+   return res
+}
+
+/*4. Given a number. Write a function that calculates its sum of the digits and if that sum
+has more than 1 digit find the sum of digits of that number. Repeat that process if needed and
+return the result.*/
+
+function getSum(n) {
+    let sum = 0
+    while (n > 0 || sum > 9) {
+         if(n === 0) {
+            n = sum
+            sum = 0
+         }
+         sum = sum + n % 10
+         n = Math.floor(n / 10)
+    }
+    return sum
+}
+
+/*5. Write a recursive function which receives a number as arguments and returns the
+fibonacci sequence as array.*/
+
+function fibonacciSequence(num){
+    let arr = [1,1]
+    if(typeof num === 'undefined'){
+        return []
+        } else if(num < 2){
+        for(let j = 0; arr.length !== num; j++){
+            arr.pop()
+        }
+    } else {
+       for(let i = 0; arr.length < num; i++){
+           arr.push(arr[i] + arr[i + 1])
+       }
+    }
+    return arr
+}
