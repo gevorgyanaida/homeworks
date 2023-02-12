26150782a0998708bb17dd82ea6a508092737f17
gevorgyanaida <99826164+gevorgyanaida@users.noreply.github.com>
2023-02-12 20:40:43 +0400

    Update Homework6.js

--- Homework6.js
+++ Homework6.js
@@ -48,3 +48,33 @@ function myBinarySearch(arr, x){
         return myBinarySearch(arr.slice(0, middleIndex), x)
     }
 }
+
+function binarySearch(arr, target, min, max){
+    let mid = Math.floor((min + max)/2)
+    if(target === arr[mid]){
+        return mid
+    }
+    if(min >= max){
+       return -1 
+    }
+    if(target > arr[mid]){
+        return binarySearch(arr, target, mid + 1, max)
+    } else if(target < arr[mid]){
+       return binarySearch(arr, target, min, mid - 1) 
+    }
+}
+
+function binarySearchWithIteration(arr, target, min, max){
+    while(min <= max){
+        let mid = Math.floor((min + max)/2)
+        if(target === arr[mid]){
+            return mid
+        }
+        if(target > arr[mid]){
+            min = mid + 1
+        } else if(target < arr[mid]){
+            max = mid - 1
+        }
+    }
+    return -1
+}
